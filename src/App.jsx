1a80bcc7797209d4d48581439ffd9a7b82eb86f0
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2024-09-15 00:57:47 +0530

    structure update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from './sections/Navbar'
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
+         <div className="max-w-7xl mx-auto">
+                   <div className="felx justify-between
+                   items-center py-5 mx-auto c-space">
+                     <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+                        Aniket </a>
+                    <button>
+                         <img src="" alt="" />                        
+                    </button>
+                   </div>
+         </div>
+    </header>
+  )
+}
+
+export default Navbar
